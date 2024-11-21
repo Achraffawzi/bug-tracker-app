116546576c12c392cdb5f46c3f3a8a4e40a8f2a5
achraffawzi <achraffawzi224a@gmail.com>
2024-11-21 11:37:44 +0100

    initial database configuration

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      default: "/avatar.png",
+    },
+    isOrganization: {
+      type: Boolean,
+      default: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", User);
